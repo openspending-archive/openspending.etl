71766a485a2c82a0ffaa7351b77b0fd823405826
Nick Stenning <nick@whiteink.com>
2011-09-16 16:37:56 +0100

    Allow unique_keys which reference subkeys of a classifier dimension

--- etl/validation/model.py
+++ etl/validation/model.py
@@ -20,8 +20,9 @@ def make_validator():
     return Model(name='model', validator=Function(_validate))
 
 def _validate(model):
-    # Ensure that all unique_keys are valid dimension names
-    unique_keys = set(model['dataset']['unique_keys'])
+    # Ensure that all unique_keys' first components (i.e. 'section' for
+    # a unique_keys entry of 'section.label') are fields in the mapping.
+    unique_keys = set(m.split('.')[0] for m in model['dataset']['unique_keys'])
     dimensions = set(model['mapping'].keys())
     if not unique_keys <= dimensions:
         return ("Invalid unique keys: %s -- unique keys "
