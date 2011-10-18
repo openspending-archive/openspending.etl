d2142ada6fd74634f292c5ab0f82d4ab564e66ee
Nick Stenning <nick@whiteink.com>
2011-10-18 15:48:14 +0100

    Rename db config option sqlalchemy -> openspending.db

--- etl/ui/config/environment.py
+++ etl/ui/config/environment.py
@@ -88,7 +88,7 @@ def load_environment(global_conf, app_conf):
         template_paths, auto_reload=True, callback=template_loaded)
 
     # SQLAlchemy
-    engine = engine_from_config(config, 'sqlalchemy.')
+    engine = engine_from_config(config, 'openspending.db.')
     engine = construct_engine(engine)
     init_model(engine)
 
