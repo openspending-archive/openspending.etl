12b0d86fb5ad5741dc01cfabd653155e186e335f
Friedrich Lindenberg <friedrich@pudo.org>
2011-11-12 22:59:55 +0100

    list some reserved names.

--- etl/validation/base.py
+++ etl/validation/base.py
@@ -2,7 +2,12 @@ from colander import (Boolean, Invalid, Function, Mapping, MappingSchema,
                       OneOf, Regex, SchemaNode, SequenceSchema, String,
                       required, null)
 
-PLACEHOLDER = "(Empty)"
+RESERVED_TERMS = ['entry', 'entries', 'dataset', 'datasets', 'dimension',
+                  'dimensions', 'editor', 'meta', 'id', 'login', 'logout',
+                  'settings', 'browser', 'explorer', 'member', 'register',
+                  'after_login', 'after_logout', 'locale', 'reporterror',
+                  'getinvolved', 'api', '500', 'error']
+
 
 class PreservingMappingSchema(MappingSchema):
     @classmethod
