2f4624a4186d0c6dc4a26f1d3a983c4c9e94062a
Friedrich Lindenberg <friedrich@pudo.org>
2011-11-14 10:22:40 +0100

    remove old-style common classes.

--- etl/validation/base.py
+++
@@ -1,8 +0,0 @@
-from colander import (Boolean, Invalid, Function, Mapping, MappingSchema,
-                      OneOf, Regex, SchemaNode, SequenceSchema, String,
-                      required, null)
-
-class PreservingMappingSchema(MappingSchema):
-    @classmethod
-    def schema_type(cls):
-        return Mapping(unknown='preserve')
