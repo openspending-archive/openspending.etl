2ad84f5894d10d3d085450da81ddf07a4e471b3d
Friedrich Lindenberg <friedrich@pudo.org>
2011-11-14 10:23:00 +0100

    extend the model and adapt the dependent tests.

--- etl/test/unit/validation/test_common.py
+++ etl/test/unit/validation/test_common.py
@@ -8,14 +8,16 @@ class TestValidationState(TestCase):
 
     def test_list_attributes(self):
         attributes = list(self.state.attributes)
-        assert len(attributes)==7, attributes
+        assert len(attributes)==11, attributes
         assert 'amount' in attributes, attributes
         assert 'function.label' in attributes, attributes
         assert not 'foo' in attributes, attributes
-    
+
     def test_list_dimensions(self):
         dimensions = list(self.state.dimensions)
-        assert len(dimensions)==2, dimensions
+        assert len(dimensions)==4, dimensions
         assert 'amount' not in dimensions, dimensions
         assert 'function' in dimensions, dimensions
         assert not 'foo' in dimensions, dimensions
+
+
