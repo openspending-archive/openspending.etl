4be105a223570fe06b43751ed75c9603aefae238
Friedrich Lindenberg <friedrich@pudo.org>
2011-11-14 10:23:10 +0100

    clean unused imports.

--- etl/test/unit/validation/test_types.py
+++ etl/test/unit/validation/test_types.py
@@ -1,8 +1,3 @@
-from openspending.etl.validation.mapping import (
-    Dimension, AmountDimension, DateDimension, Field, Fields, Mapping
-)
-
-
 from ... import TestCase, helpers as h
 from openspending.etl.validation import types
 
